a192a93dc0879751722c789cb612a7120e287f1d
Mroik <mroik@delayed.space>
2024-04-12 17:41:29 +0200

    Small fix

--- userbot.py
+++ userbot.py
@@ -11,7 +11,7 @@ client.parse_mode = "markdown"
 async def check_message(event):
     message: Message = event.message
     c: Chat = await message.get_chat()
-    found = len(list(filter(lambda x: x in message.message, WORDLIST))) > 0
+    found = len(list(filter(lambda x: x in message.message.lower(), WORDLIST))) > 0
     if found:
         print(f"[{message.chat_id}](https://t.me/c/{message.chat_id}/{message.id})")
         to_send = f"[{message.from_id.user_id}](tg://user?id={message.from_id.user_id}) "

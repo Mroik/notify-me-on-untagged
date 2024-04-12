ec3f1a80a064eadf91b58c9a3f309b1ddc42fd5a
Mroik <mroik@delayed.space>
2024-04-12 19:22:52 +0200

    Do not forward if mentioned

--- userbot.py
+++ userbot.py
@@ -12,7 +12,7 @@ async def check_message(event):
     message: Message = event.message
     c: Chat = await message.get_chat()
     found = len(list(filter(lambda x: x in message.message.lower(), WORDLIST))) > 0
-    if found:
+    if found and not message.mentioned:
         print(f"[{message.chat_id}](https://t.me/c/{message.chat_id}/{message.id})")
         to_send = f"[{message.from_id.user_id}](tg://user?id={message.from_id.user_id}) "
         to_send += f"tagged you in [{c.id}](https://t.me/c/{c.id}/{message.id})"
